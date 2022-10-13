7493bfefbe908d5b5f2d30b8b8e1ab10bafe5bf8
kadenzipfel <kaden.zipfel@hotmail.com>
2022-10-13 14:51:28 -0700

    Fix broken logic

--- src/lib/HourglassMath.sol
+++ src/lib/HourglassMath.sol
@@ -23,20 +23,19 @@ library HourglassMath {
     /// @param marketSpan total market open time span (in seconds)
     /// @return tokenXReserves reserves for token X
     function tokenXReservesAtTokenYReserves(
-        uint256 tokenYReserves,
-        uint256 liquidity,
-        uint256 timeRemaining,
-        uint256 marketSpan
-    ) public pure returns (uint128) {
+        int128 tokenYReserves,
+        int128 liquidity,
+        int128 timeRemaining,
+        int128 marketSpan
+    ) public pure returns (int128) {
         if (tokenYReserves == 0 || liquidity == 0) revert ZeroValue();
 
         // 1 - 1/√tₘ
-        uint128 z = _calculateZ(_calculateTm(timeRemaining, marketSpan));
+        int128 z = _calculateZ(_calculateTm(timeRemaining, marketSpan));
 
         // (1 - 1/√tₘ)√(2L^(1 - 1/√tₘ)-y^(1 - 1/√tₘ))
-        return pow(
-            sub(pow(fromInt(int256(mul(int128(2), int128(liquidity)))), z), pow(fromUInt(tokenYReserves), z)),
-            div(int128(1), z)
+        return int256(int128(2).mul(int128(liquidity))).fromInt().pow(z).sub(
+            tokenYReserves.fromInt().pow(z).fromInt().pow(int128(1).div(z))
         );
     }
 
@@ -44,14 +43,14 @@ library HourglassMath {
     /// @param timeRemaining time remaining until market maturity (in seconds)
     /// @param marketSpan total market open time span (in seconds)
     /// @return tm fraction of time remaining in market open
-    function _calculateTm(uint256 timeRemaining, uint256 marketSpan) private pure returns (uint128) {
-        return uint128(div(timeRemaining.fromUInt(), marketSpan.fromUInt()));
+    function _calculateTm(int128 timeRemaining, int128 marketSpan) private pure returns (uint128) {
+        return uint128(timeRemaining.fromInt().div(marketSpan.fromInt()));
     }
 
     /// @dev Calculate shared exponent z (1 - 1/√tₘ)
     /// @param tm fraction of time remaining in the market open
     /// @return z shared exponent of invariant
-    function _calculateZ(uint128 tm) private pure returns (uint128) {
-        return uint128(sub(int128(1), div(int128(1), sqrt(int128(tm)))));
+    function _calculateZ(uint128 tm) private pure returns (int128) {
+        return int128(1).sub(int128(1).div(int128(tm).sqrt()));
     }
 }
