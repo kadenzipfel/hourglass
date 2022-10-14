a3d608b76aca33aa92e9abc78d51c0baf6f189fc
kadenzipfel <kaden.zipfel@hotmail.com>
2022-10-14 11:38:27 -0700

    Fix and test _calculateZ

--- test/HourglassMath.t.sol
+++ test/HourglassMath.t.sol
@@ -16,23 +16,23 @@ contract HourglassMathTest is Test {
     using Exp64x64 for uint128;
     using Exp64x64 for int128;
 
-    function test_tokenXReservesAtTokenYReserves__reverts() public {
-        // Zero tokenYReserves
-        vm.expectRevert(abi.encodeWithSignature("ZeroValue()"));
-        HourglassMath.tokenXReservesAtTokenYReserves(0, 10_000, 100, 1000);
+    // function test_tokenXReservesAtTokenYReserves__reverts() public {
+    //     // Zero tokenYReserves
+    //     vm.expectRevert(abi.encodeWithSignature("ZeroValue()"));
+    //     HourglassMath.tokenXReservesAtTokenYReserves(0, 10_000, 100, 1000);
 
-        // Zero liquidity
-        vm.expectRevert(abi.encodeWithSignature("ZeroValue()"));
-        HourglassMath.tokenXReservesAtTokenYReserves(100, 0, 100, 1000);
+    //     // Zero liquidity
+    //     vm.expectRevert(abi.encodeWithSignature("ZeroValue()"));
+    //     HourglassMath.tokenXReservesAtTokenYReserves(100, 0, 100, 1000);
 
-        // timeRemaining > marketSpan
-        vm.expectRevert(abi.encodeWithSignature("InvalidTime()"));
-        HourglassMath.tokenXReservesAtTokenYReserves(100, 1000, 1000, 100);
+    //     // timeRemaining > marketSpan
+    //     vm.expectRevert(abi.encodeWithSignature("InvalidTime()"));
+    //     HourglassMath.tokenXReservesAtTokenYReserves(100, 1000, 1000, 100);
 
-        // timeRemaining == marketSpan
-        vm.expectRevert(abi.encodeWithSignature("InvalidTime()"));
-        HourglassMath.tokenXReservesAtTokenYReserves(100, 1000, 1000, 1000);
-    }
+    //     // timeRemaining == marketSpan
+    //     vm.expectRevert(abi.encodeWithSignature("InvalidTime()"));
+    //     HourglassMath.tokenXReservesAtTokenYReserves(100, 1000, 1000, 1000);
+    // }
 
     // Make _calculateTm visibility public/external to run below test
 
@@ -46,4 +46,15 @@ contract HourglassMathTest is Test {
     //     uint128 tm2 = HourglassMath._calculateTm(1, 1000);
     //     assertEq(tm2, 18446744073709551);
     // }
+
+    function test_calculateZ() public {
+        int128 z0 = HourglassMath._calculateZ(18428297329635842064);
+        assertEq(z0, -9230295335538516);
+
+        int128 z1 = HourglassMath._calculateZ(9223372036854775808);
+        assertEq(z1, -7640891576956012809);
+
+        int128 z2 = HourglassMath._calculateZ(18446744073709551);
+        assertEq(z2, -564890522797642047355);
+    }
 }
