b714d106fcd6000548fddf12d56ae7b5432a3fdc
kadenzipfel <kaden.zipfel@hotmail.com>
2022-10-14 10:55:26 -0700

    Update math, error handling

--- src/lib/HourglassMath.sol
+++ src/lib/HourglassMath.sol
@@ -5,6 +5,7 @@ import {Exp64x64} from "./Exp64x64.sol";
 import {Math64x64} from "./Math64x64.sol";
 
 error ZeroValue();
+error InvalidTime();
 
 /// @dev Math library for Hourglass logic
 /// @author https://github.com/kadenzipfel
@@ -16,6 +17,8 @@ library HourglassMath {
     using Exp64x64 for uint128;
     using Exp64x64 for int128;
 
+    uint128 public constant ONE = 0x10000000000000000; //   In 64.64
+
     /// @notice Calculates token X reserves given token Y reserves
     /// @param tokenYReserves reserves of token Y
     /// @param liquidity amount of liquidity in pool
@@ -23,23 +26,28 @@ library HourglassMath {
     /// @param marketSpan total market open time span (in seconds)
     /// @return tokenXReserves reserves for token X
     function tokenXReservesAtTokenYReserves(
-        uint256 tokenYReserves,
-        uint256 liquidity,
+        uint128 tokenYReserves,
+        uint128 liquidity,
         int128 timeRemaining,
         int128 marketSpan
-    ) public pure returns (int128) {
+    ) public pure returns (uint128) {
         if (tokenYReserves == 0 || liquidity == 0) revert ZeroValue();
+        if (timeRemaining >= marketSpan) revert InvalidTime();
 
         // 1 - 1/√tₘ
-        int128 z = _calculateZ(_calculateTm(timeRemaining, marketSpan));
+        uint128 z = _calculateZ(_calculateTm(timeRemaining, marketSpan));
 
-        int128 one64 = int256(1).fromInt();
-        int128 two64 = int256(2).fromInt();
-        int128 liquidity64 = liquidity.fromUInt();
-        int128 tokenYReserves64 = tokenYReserves.fromUInt();
+        // 2L^(1 - 1/√tₘ)
+        uint256 l = (int128(2*ONE)).mulu(uint256(liquidity.pow(z, ONE)));
+
+        // y^(1 - 1/√tₘ)
+        uint256 y = tokenYReserves.pow(z, ONE);
+
+        // 2L^(1 - 1/√tₘ)-y^(1 - 1/√tₘ)
+        uint256 ly = l - y;
 
         // (1 - 1/√tₘ)√(2L^(1 - 1/√tₘ)-y^(1 - 1/√tₘ))
-        return two64.mul(liquidity64.pow(z)).sub(tokenYReserves64.pow(z)).pow(one64.div(z));
+        return uint128(ly).pow(uint128(int128(ONE).div(int128(z))), ONE);
     }
 
     /// @dev Calculate time to maturity fraction (tₘ)
@@ -53,7 +61,7 @@ library HourglassMath {
     /// @dev Calculate shared exponent z (1 - 1/√tₘ)
     /// @param tm fraction of time remaining in the market open
     /// @return z shared exponent of invariant
-    function _calculateZ(uint128 tm) private pure returns (int128) {
-        return int128(1).sub(int128(1).div(int128(tm).sqrt()));
+    function _calculateZ(uint128 tm) private pure returns (uint128) {
+        return uint128(int128(ONE).sub(int128(ONE).div(int128(tm).sqrt())));
     }
 }
